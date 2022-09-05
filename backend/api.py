07fcdcb3c386486a769010ee0e6813431cbf31a3
Arsene Awounou <developer.awounou@gmail.com>
2022-09-06 00:20:39 +0100

    database configs

---
+++ backend/api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Tasky API
+"""
+from flask import Flask
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from os import getenv
+
+
+app = Flask(__name__)
+app.secret_key = b"\xb8\x17\xba\xf9\xc0\x02\xff\xa4'\xee\xbe\xe75 ,r"
+
+load_dotenv()
+
+URI = (f"mongodb+srv://{getenv('MONGODB_USR')}:{getenv('MONGOBD_PWD')}"
+       "@cluster0.erimhkb.mongodb.net/?retryWrites=true&w=majority")
+
+cluster = MongoClient(URI)
+db = cluster[getenv('MONGODB_DB')]
+
+
+if __name__ == '__main__':
+    app.run()
